762e459b1d1249fd99b359d7f5fd81dc2c4a3f1a
Kevin Neira <144465846+k3vineira@users.noreply.github.com>
2026-03-12 14:30:22 -0500

    initial commit

---
+++ inicio/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def inicio_inicio(request):
+    context={
+        'nombre': 'kevin neira',
+        'titulo': 'Página de Inicio'
+    }
+    return render(request, 'inicio/index.html', context)
