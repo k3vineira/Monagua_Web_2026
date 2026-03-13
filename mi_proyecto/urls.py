485869de07425a2e4b3f19060b2ebc61f68db191
VICTOR <vt74117@gmail.com>
2026-03-13 17:44:34 -0500

    agrego los datos faltantes
    ya con esto ya estaria el codigó funcionando completamente

--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inicio.urls')), # Conecta las URLs de tu app
+    path('usuarios/', include('usuarios.urls')), # Conecta las URLs de la app usuarios
 ]
