add1db1e8b180673789ff276e593648d2b95844d
Kevin Neira <neirak424@gmail.com>
2026-04-19 12:21:31 -0500

    feat: añadir vistas y rutas para el dashboard de guías y mejorar el menú de usuario

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from panel import views as views_gestion
+from usuario import views as views_usuario
 from . import views
  
 urlpatterns = [
@@ -24,6 +25,9 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='inicio_sesion.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('gestion-admin/', views_gestion.dashboard_administrador, name='gestion_admin'),
+    
+    # Dashboard de Guía
+    path('inicio-guia/', views_usuario.dashboard_guia_view, name='inicio_guia'),
     
     # 4. Recuperación de contraseña
     path('recuperar-password/', auth_views.PasswordResetView.as_view(template_name='recuperar.html'), name='password_reset'),
