f92a199a5a784eb1a28c27e44c3f48785503b1af
VICTOR <vt74117@gmail.com>
2026-04-22 13:26:28 -0500

    Add Guías Management and Promotions Management templates
    - Created index-guias.html for managing local guides, including KPI cards, filters, and modals for adding/editing guides.
    - Implemented promociones_gestion.html for managing promotions with a responsive layout, including modals for creating and editing promotions.

--- core/settings.py
+++ core/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # La carpeta final para la nube
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
